8463e78049dc5dc687697647ad880253d3e5451f
milkomoreno <milkomoreno@MILKOMORENOE19B>
2021-10-11 19:27:32 -0400

    Add project files.

---
+++ Estudio/Data/EstudioDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Estudio.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Estudio.Data
+{
+    public class EstudioDbContext: DbContext
+    {
+        public DbSet<Music> Songs{ get; set; } //pq no hay plural de music (songs deberia ser music)
+
+
+        public EstudioDbContext(DbContextOptions<EstudioDbContext>options): base(options)//enlaza la base de datos c/ el entity framework 
+
+        {
+                
+        }
+    }
+}
---
+++ Estudio/Models/Music.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Estudio.Models
+{
+        public class Music
+    {
+        [Key]
+        public int MusicId { get; set; }
+        
+        
+        
+        [Required(ErrorMessage = "el nombre de la cancion es requerido")]
+        [StringLength(60, MinimumLength = 5, ErrorMessage = "el nombre de la cancion debe estar entre 5 y 60 caracteres")]
+        [Display (Name = "nombre de la cancion")]
+        public string Nombre { get; set; }
+       
+        
+     
+        [Required(ErrorMessage = "el nombre del autor es requerido")]
+        [StringLength(60, MinimumLength = 5, ErrorMessage = "el nombre del autor debe estar entre 5 y 60 caracteres")]
+        [Display(Name = "nombre del autor")]
+        public string Autor { get; set; }
+
+
+        [Required(ErrorMessage = "letra de la cancion")]
+        [StringLength(60, MinimumLength = 5, ErrorMessage = "letra de la cancion")]
+        [Display(Name = "link de la letra  de la cancion")]
+        public string Letra { get; set; }
+    }
+}
